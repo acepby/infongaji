0d959a8da55d809a81bc0d7921d43990ed2dfa7e
Adi Sucipto <acepby@gmail.com>
2019-05-28 08:02:51 +0700

    add dbhelper

---
+++ dbhelper.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+
+class DBHelper:
+
+	def __init__(self, dbname="ngaji.sqlite"):
+		self.dbname = dbname
+		self.conn = sqlite3.connect(dbname,check_same_thread=False)
+
+	def setup(self):
+		tblstmt = "CREATE TABLE IF NOT EXISTS ngaji ( id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, agenda text,pembicara text,materi text,lokasi text,hari text,tanggal text,waktu text,host text,lat text,lon text,owner text )"
+		itemidx = "CREATE INDEX IF NOT EXISTS ngajiIndex ON ngaji (id ASC)" 
+		ownidx = "CREATE INDEX IF NOT EXISTS ownIndex ON ngaji (owner ASC)"
+		self.conn.execute(tblstmt)
+		self.conn.execute(itemidx)
+		self.conn.execute(ownidx)
+		self.conn.commit()
+
+	def add_info(self,agd,ust,hal,loc,hr,tgl,jam,host,lat,lon,owner):
+		stmt = "INSERT INTO ngaji VALUES(null,?,?,?,?,?,?,?,?,?,?,?)"
+		args = (agd,ust,hal,loc,hr,tgl,jam,host,lat,lon,owner)
+		try :
+			self.conn.execute(stmt, args)
+			self.conn.commit()
+		except sqlite3.Error as er:
+			print('er: ',er)
+
+	def delete_info(self, id, owner):
+		stmt = "DELETE FROM ngaji WHERE id = (?) AND owner = (?)"
+		args = (id, owner )
+		self.conn.execute(stmt, args)
+		self.conn.commit()
+
+	def get_info(self, owner):
+		stmt = "SELECT agenda,pembicara,materi FROM ngaji WHERE owner = (?)"
+		args = (owner, )
+		return [x[0] for x in self.conn.execute(stmt, args)]
+	def get_sepekan(self):
+		stmt = "SELECT id,agenda FROM ngaji WHERE tanggal NOT NULL  AND (strftime('%W',tanggal)=strftime('%W','now'))"
+		data = self.conn.execute(stmt)
+		all = data.fetchall()
+		if not all:
+			print("data tidak ditemukan")
+		else : 
+			return all
